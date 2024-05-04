010c8bc1ebd62566912d30463eef6e11a31fc21a
zjun <565061763@qq.com>
2024-05-04 16:53:44 +0800

    SampleLifecycle

--- app/src/main/java/com/sd/demo/compose/core/sample/SampleLifecycle.kt
+++ app/src/main/java/com/sd/demo/compose/core/sample/SampleLifecycle.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
 import androidx.lifecycle.lifecycleScope
 import com.sd.demo.compose.core.logMsg
 import com.sd.demo.compose.core.theme.AppTheme
 import com.sd.lib.compose.core.fAppIsStartedFlow
+import com.sd.lib.compose.core.fAppLifecycleScope
+import com.sd.lib.compose.core.fAwait
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SampleLifecycle : ComponentActivity() {
@@ -19,6 +24,10 @@ class SampleLifecycle : ComponentActivity() {
             AppTheme {
                 Content()
             }
+        }
+
+        fAppLifecycleScope.launch {
+            logMsg { "fAppLifecycleScope launched" }
         }
 
         lifecycleScope.launch {
@@ -36,5 +45,14 @@ private fun Content() {
     }
     LifecycleEventEffect(Lifecycle.Event.ON_STOP) {
         logMsg { "ON_STOP" }
+    }
+
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    LaunchedEffect(lifecycle) {
+        while (true) {
+            lifecycle.fAwait()
+            logMsg { "lifecycle" }
+            delay(1_000)
+        }
     }
 }
