6d56484dd9e87ccbd60468a4c487b1f62e2bae29
zjun <565061763@qq.com>
2024-05-23 17:02:31 +0800

    对齐线支持显示颜色

--- lib/src/main/java/com/sd/lib/compose/core/utils/ConstraintLayout.kt
+++ lib/src/main/java/com/sd/lib/compose/core/utils/ConstraintLayout.kt
@@ -1,10 +1,14 @@
 package com.sd.lib.compose.core.utils
 
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstrainedLayoutReference
 import androidx.constraintlayout.compose.ConstraintLayoutScope
+import androidx.constraintlayout.compose.Dimension
 
 /**
  * 横向参考线，距离目标[target]的top百分比[bias]，如果[target]为null默认为parent
@@ -13,16 +17,35 @@ import androidx.constraintlayout.compose.ConstraintLayoutScope
 fun ConstraintLayoutScope.fHorizontalLine(
     bias: Float,
     target: ConstrainedLayoutReference? = null,
+    color: Color? = null,
 ): ConstrainedLayoutReference {
     return createRef().also { line ->
-        Spacer(modifier = Modifier.constrainAs(line) {
-            val finalTarget = target ?: parent
-            linkTo(
-                top = finalTarget.top,
-                bottom = finalTarget.bottom,
-                bias = bias,
-            )
-        })
+        Box(
+            modifier = Modifier
+                .constrainAs(line) {
+                    val finalTarget = target ?: parent
+                    linkTo(
+                        top = finalTarget.top,
+                        bottom = finalTarget.bottom,
+                        bias = bias,
+                    )
+                    if (color != null) {
+                        height = Dimension.value(1.dp)
+                        linkTo(
+                            start = finalTarget.start,
+                            end = finalTarget.end,
+                        )
+                        width = Dimension.fillToConstraints
+                    }
+                }
+                .let {
+                    if (color == null) {
+                        it
+                    } else {
+                        it.background(color)
+                    }
+                }
+        )
     }
 }
 
@@ -33,15 +56,34 @@ fun ConstraintLayoutScope.fHorizontalLine(
 fun ConstraintLayoutScope.fVerticalLine(
     bias: Float,
     target: ConstrainedLayoutReference? = null,
+    color: Color? = null,
 ): ConstrainedLayoutReference {
     return createRef().also { line ->
-        Spacer(modifier = Modifier.constrainAs(line) {
-            val finalTarget = target ?: parent
-            linkTo(
-                start = finalTarget.start,
-                end = finalTarget.end,
-                bias = bias,
-            )
-        })
+        Box(
+            modifier = Modifier
+                .constrainAs(line) {
+                    val finalTarget = target ?: parent
+                    linkTo(
+                        start = finalTarget.start,
+                        end = finalTarget.end,
+                        bias = bias,
+                    )
+                    if (color != null) {
+                        width = Dimension.value(1.dp)
+                        linkTo(
+                            top = finalTarget.top,
+                            bottom = finalTarget.bottom,
+                        )
+                        height = Dimension.fillToConstraints
+                    }
+                }
+                .let {
+                    if (color == null) {
+                        it
+                    } else {
+                        it.background(color)
+                    }
+                }
+        )
     }
 }
