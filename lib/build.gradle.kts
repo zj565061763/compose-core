c8c3d9075989ced68ef22e45617ea3a0ed62ae65
zjun <565061763@qq.com>
2024-05-23 17:03:17 +0800

    1.0.4

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-core"
-val libVersion = "1.0.3"
+val libVersion = "1.0.4"
 
 android {
     namespace = "com.sd.lib.compose.core"
