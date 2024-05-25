59805131d1f226ae4777a53b236b459d5a6750c0
zjun <565061763@qq.com>
2024-05-26 01:20:54 +0800

    demo

--- app/src/main/java/com/sd/demo/compose/core/sample/utils/SampleDecayIndexLooper.kt
+++ app/src/main/java/com/sd/demo/compose/core/sample/utils/SampleDecayIndexLooper.kt
@@ -5,7 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -14,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.sd.demo.compose.core.theme.AppTheme
 import com.sd.lib.compose.core.utils.FDecayIndexLooper
 import kotlinx.coroutines.launch
@@ -44,8 +49,9 @@ private fun Content(
     ) {
         ListView(
             selectedIndex = looper.currentIndex,
-            modifier = Modifier.weight(1f)
         )
+
+        Spacer(modifier = Modifier.height(20.dp))
 
         Row {
             Button(onClick = {
@@ -54,8 +60,10 @@ private fun Content(
                 Text(text = "开始循环")
             }
 
+            Spacer(modifier = Modifier.width(10.dp))
+
             Button(onClick = {
-                coroutineScope.launch { looper.startDecay(2) }
+                coroutineScope.launch { looper.startDecay(3) }
             }) {
                 Text(text = "开始减速")
             }
@@ -69,7 +77,7 @@ private fun ListView(
     selectedIndex: Int,
 ) {
     LazyColumn(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         items(COUNT) { index ->
