f85b22ad3f994bbf159f918ac393f238b460bed2
zjun <565061763@qq.com>
2024-05-26 23:54:21 +0800

    闪烁逻辑优化

--- lib/src/main/java/com/sd/lib/compose/core/utils/Flicker.kt
+++ lib/src/main/java/com/sd/lib/compose/core/utils/Flicker.kt
@@ -1,6 +1,7 @@
 package com.sd.lib.compose.core.utils
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -12,49 +13,54 @@ import androidx.compose.ui.graphics.graphicsLayer
 fun Modifier.fFlicker(
     /** 是否闪烁 */
     flicker: Boolean,
+    /** 是否可用 */
+    enabled: Boolean = true,
     /** 开始透明度 */
     startAlpha: Float = 1f,
     /** 结束透明度 */
     endAlpha: Float = 0f,
     /** 闪烁重复次数 */
     repeatCount: Int = 2,
-    /** 是否可用 */
-    enabled: Boolean = true,
+    /** 执行动画到开始透明度 */
+    animateToStartAlpha: suspend (Animatable<Float, AnimationVector1D>, Float) -> Unit = { animatable, alpha ->
+        animatable.animateTo(alpha)
+    },
+    /** 执行动画到结束透明度 */
+    animateToEndAlpha: suspend (Animatable<Float, AnimationVector1D>, Float) -> Unit = { animatable, alpha ->
+        animatable.animateTo(alpha)
+    },
     /** 闪烁结束回调 */
     onFlickerFinish: () -> Unit,
-): Modifier = this.composed {
-    require(repeatCount > 0)
+): Modifier = if (enabled) {
+    composed {
+        val startAlphaUpdated by rememberUpdatedState(startAlpha)
+        val endAlphaUpdated by rememberUpdatedState(endAlpha)
+        val repeatCountUpdated by rememberUpdatedState(repeatCount)
 
-    val startAlphaUpdated by rememberUpdatedState(startAlpha)
-    val endAlphaUpdated by rememberUpdatedState(endAlpha)
-    val repeatCountUpdated by rememberUpdatedState(repeatCount)
+        val animatable = remember { Animatable(1f) }
+        val onFlickerFinishUpdated by rememberUpdatedState(onFlickerFinish)
 
-    val animatable = remember { Animatable(1f) }
-    val onFlickerFinishUpdated by rememberUpdatedState(onFlickerFinish)
-
-    if (enabled && flicker) {
-        LaunchedEffect(Unit) {
-            animatable.snapTo(startAlphaUpdated)
-            if (repeatCountUpdated == Int.MAX_VALUE) {
-                while (true) {
-                    animatable.animateTo(endAlphaUpdated)
-                    animatable.animateTo(startAlphaUpdated)
+        if (flicker) {
+            LaunchedEffect(animatable) {
+                animatable.snapTo(startAlphaUpdated)
+                if (repeatCountUpdated == Int.MAX_VALUE) {
+                    while (true) {
+                        animateToEndAlpha(animatable, endAlphaUpdated)
+                        animateToStartAlpha(animatable, startAlphaUpdated)
+                    }
+                } else {
+                    repeat(repeatCountUpdated) {
+                        animateToEndAlpha(animatable, endAlphaUpdated)
+                        animateToStartAlpha(animatable, startAlphaUpdated)
+                    }
+                    onFlickerFinishUpdated()
                 }
-            } else {
-                repeat(repeatCountUpdated) {
-                    animatable.animateTo(endAlphaUpdated)
-                    animatable.animateTo(startAlphaUpdated)
-                }
-                onFlickerFinishUpdated()
             }
         }
-    }
-
-    if (enabled) {
         graphicsLayer {
             this.alpha = animatable.value
         }
-    } else {
-        this@fFlicker
     }
+} else {
+    this
 }
